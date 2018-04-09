702c8c6bff7ebad4a11002e0b94f99457d3cc555
Liqiaomiao <1049259465@qq.com>
2018-04-09 16:19:27 +0800

    jqgrid常用属性、合并单元格总结

---
+++ 报表test/data/basicprice.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Liqiaomiao on 2018/3/27.
+ */
+var price={"obj":[{"isShow":1,"price":100},{"isShow":1,"price":200},{"isShow":1,"price":300},{"isShow":1,"price":400}],"status":true}
